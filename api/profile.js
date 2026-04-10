2d223f7b32597a54653a51f5518fcb300235d244
xuan <zyxlcr@qq.com>
2026-04-10 12:35:03 +0800

    Update profile.js

--- api/profile.js
+++ api/profile.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 const cookie = require('cookie');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
+//export default async function handler(req, res) {
     if (req.method !== 'GET') {
         return res.status(405).json({ error: 'Method not allowed' });
     }
